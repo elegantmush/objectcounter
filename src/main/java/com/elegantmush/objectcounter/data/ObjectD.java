ac8cc9fb70c730f12c25df9b1f544287671fe76a
Umesh Telang <elegantmush@gmail.com>
2013-04-28 18:51:16 +0100

    Adding constructor chaining within ObjectD to ensure it doesn't get added as multiple instances. Verified by the integration test ObjectCounterIT

--- src/main/java/com/elegantmush/objectcounter/data/ObjectD.java
+++ src/main/java/com/elegantmush/objectcounter/data/ObjectD.java
@@ -2,4 +2,28 @@ package com.elegantmush.objectcounter.data;
 
 public class ObjectD {
 
+	private String s;
+	private int i;
+
+	public ObjectD() {
+		this("", 0);
+	}
+
+	public ObjectD(String s) {
+		this(s, 0);
+	}
+
+	public ObjectD(String s, int i) {
+		this.s = s;
+		this.i = i;
+	}
+
+	public String getS() {
+		return s;
+	}
+
+	public int getI() {
+		return i;
+	}
+
 }
